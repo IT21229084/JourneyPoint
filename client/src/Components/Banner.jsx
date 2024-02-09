1085434166872104515ee37b88b318e54331cfdb
Malith Iroshan <99582850+IT21229084@users.noreply.github.com>
2024-02-09 11:57:25 +0530

    commit

--- client/src/Components/Banner.jsx
+++ client/src/Components/Banner.jsx
@@ -17,9 +17,9 @@ const Banner = ({ query, handleInputChange }) => {
                         <FiSearch className='absolute mt-2.5 ml-2 text-gray-400' />
                     </div>
                     <div className='flex md:round-s-none rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/3 w-full'>
-                        <input type='text' placeholder='Location' name='title' id='title' className='black flex-1 border-0 bg-transparent
+                        <input type='text' placeholder='Location' name='location' id='location' className='black flex-1 border-0 bg-transparent
                         py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:right-0 sm:text-sm:leading-6'
-                            value={""} />
+                             />
                         <FiMapPin className='absolute mt-2.5 ml-2 text-gray-400' />
                     </div>
                     <button type='submit' className='bg-blue py-2 px-8 text-white md:rounded-s-none rounded'>Search</button>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,82 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Banner from '../Components/Banner'
 import { useState } from 'react'
+import Card from '../Components/Card'
+import Jobs from './Jobs'
 const Home = () => {
-  const [query,SetQuery] = useState("")
-  const handleInputChange = (event) =>{
-      SetQuery(event.target.value)
-      console.log(query)
-  
+  const [selectdcategory, setSelectdcategory] = useState(null)
+  const [jobs, setJobs] = useState([])
+  const [query, SetQuery] = useState("")
+
+  useEffect(() => {
+    fetch("jobs.json").then(res => res.json().then(data => {
+      setJobs(data)
+    }))
+  }, [])
+
+  const handleInputChange = (event) => {
+    SetQuery(event.target.value)
+    console.log(query)
   }
+
+  //filter jobs by jobstitle
+  const filteredItems = jobs.filter((job) =>
+    job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+
+  //....Radio Filter....
+  const handleChange = (event) => {
+    setSelectdcategory(event.target.value)
+  }
+
+  //Button filtering
+  const handleClick = (event) => {
+    setSelectdcategory(event.target.value)
+  }
+
+  //Main function
+  const filteredData = (jobs, selected, query) => {
+    let filteredJobs = jobs
+
+    //filtering Input items
+    if (query) {
+      filteredJobs = filteredItems
+    }
+
+    //category filtering
+    if (selected) {
+      filteredJobs = filteredJobs.filter(({ jobLocation, maxPrice, experienceLevel,
+        salaryType, employmentType, postingDate }
+      ) => (
+        jobLocation.toLowerCase() === selected.toLowerCase() ||
+        parseInt(maxPrice) <= parseInt(selected) ||
+        salaryType.toLowerCase() === selected.toLowerCase() ||
+        employmentType.toLowerCase() === selected.toLowerCase()
+
+      ))
+      console.log(filteredJobs)
+    }
+
+    return filteredJobs.map((data, i) => <Card key={i} data={data} />)
+  }
+
+  const result = filteredData(jobs, selectdcategory, query)
   return (
     <div>
       <Banner query={query} handleInputChange={handleInputChange} />
-      
+
+
+      {/* Main content */}
+      <div className='bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12 '>
+
+
+        <div className='bg-white p-4 rounded'>left</div>
+        <div className='col-span-2 bg-white p-4 rounded'> <Jobs result={result} /></div>
+        <div className='bg-white p-4 rounded'>right</div>
+
+
       </div>
+
+    </div>
   )
 }
-
 export default Home
